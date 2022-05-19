4ac4cd060db91cb28eb2e0994a626dd31d01178c
doniataskman <doniahammud@gmail.com>
2022-05-19 11:45:03 +0300

    dfhrfthhgd

---
+++ app/src/main/java/abbas/samih/salamalbums/FullExample/AllPicActivity.java
@@ -0,0 +1,16 @@
+package abbas.samih.salamalbums.FullExample;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+import abbas.samih.salamalbums.R;
+
+public class AllPicActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_all_pic);
+    }
+}
